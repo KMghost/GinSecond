22574f7a004f097a37ecec47d6fd17daef475bb6
Daniel <wuyh512@foxmail.com>
2021-11-19 12:10:43 +0800

    分组路由嵌套

--- RestfulAPI /04router.go
+++ RestfulAPI /04router.go
@@ -21,6 +21,8 @@ func main() {
 		})
 	}
 
+	// ========================================================================
+	// 路由中间件
 	v2Group := r.Group("/v2", func(c *gin.Context) {
 		fmt.Println("中间件")
 	})
@@ -30,6 +32,18 @@ func main() {
 		})
 		v2Group.GET("/products", func(c *gin.Context) {
 			c.String(200, "/v2/products")
+		})
+	}
+
+	// ========================================================================
+	// 分组路由嵌套
+	v1AdminGroup := v1Group.Group("/admin")
+	{
+		v1AdminGroup.GET("/users", func(c *gin.Context) {
+			c.String(200, "/v1/admin/users")
+		})
+		v1AdminGroup.GET("/manager", func(c *gin.Context) {
+			c.String(200, "/v1/admin/manager")
 		})
 	}
 
